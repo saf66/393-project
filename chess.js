7f1468eb9ed415c53f889056fa7dbf0a3c8e87a4
saf66 <saf66@case.edu>
2011-12-15 00:44:08 -0500

    moved validateMove() into Game started basic network functionality (assigning user IDs) started NodeJS server (keeps track of user IDs)

--- chess.js
+++ chess.js
@@ -1,169 +1,3 @@
-
-	//given a piece and a target tuple, can that piece go there?
- function validateMove( type, here, there, color)
- {
- 	if(here[0]==there[0] && here[1]==there[1]) 
- 		return true;
-	if(Game.getPieceAt(there) != null && Game.getPieceAt(there).color == color)
-		return false;
-
- 	switch(type)
- 		{//Game.getPieceAt(grid)
- 			case("rook"):{
- 				
- 				if(here[0]==there[0])
- 				{	if (there[1]> here[1]) var inc = -1;
- 					else var inc = 1;
- 					
- 					for(var y = there[1] + inc; y != here[1]; y += inc) 
- 						{
- 							if(Game.getPieceAt( [here[0], y]) != null)
- 								return false;
- 						}
- 					return true;
- 				}
- 				
- 				if(here[1]==there[1])
- 				{	if (there[0] > here[0]) var inc = -1;
- 					else var inc = 1;
- 					
- 					for(var x = there[0] + inc; x != here[0]; x += inc) 
- 						{
- 							if(Game.getPieceAt( [x, here[1] ]) != null)
- 								return false;
- 						}
- 					return true;
- 				}
- 				
- 				return false;
- 				break;
-
- 				}
- 			case("knight"):{
- 				if(there[0] == here[0] + 2 || there[0] == here[0] - 2)
- 						if(there[1] == here[1]+1 || there[1] == here[1]-1)
- 							return true;
- 				if(there[1] == here[1] + 2 || there[1] == here[1] - 2)
- 						if(there[0] == here[0]+1 || there[0] == here[0]-1)
- 							return true;
- 				return false;
- 				break;
- 			}
- 			case("bishop"):{
- 				
- 				if(!(Math.abs(here[0]-there[0]) == Math.abs(here[1]-there[1])))
- 					return false;
- 					
- 					if (there[1]> here[1]) var yinc = -1;
- 					else var yinc = 1;
- 					
- 					if (there[0]> here[0]) var xinc = -1;
- 					else var xinc = 1;
- 					
- 					var x = there[0] + xinc;
- 					var y = there[1] + yinc;
-
- 					while (x!= here[0] && y != here[1]) 
- 						{
- 							console.log('checking (%o,%o)', x, y);
- 							if(Game.getPieceAt( [x, y]) != null)
- 								return false;
- 								
- 								x+=xinc; y+=yinc;
- 						}
- 					return true;
- 				
- 				
- 				
- 				
- 				break;
-
- 			}
- 			case("queen"):{
- 				
- 				//rook-like movement
- 				if(here[0]==there[0])
- 				{	if (there[1]> here[1]) var inc = -1;
- 					else var inc = 1;
- 					
- 					for(var y = there[1] + inc; y != here[1]; y += inc) 
- 						{
- 							if(Game.getPieceAt( [here[0], y]) != null)
- 								return false;
- 						}
- 					return true;
- 				}
- 				
- 				if(here[1]==there[1])
- 				{	if (there[0] > here[0]) var inc = -1;
- 					else var inc = 1;
- 					
- 					for(var x = there[0] + inc; x != here[0]; x += inc) 
- 						{
- 							if(Game.getPieceAt( [x, here[1] ]) != null)
- 								return false;
- 						}
- 					return true;
- 				}
- 				//bishop-like movement
- 				 if(!(Math.abs(here[0]-there[0]) == Math.abs(here[1]-there[1])))
- 					return false;
- 					
- 					if (there[1]> here[1]) var yinc = -1;
- 					else var yinc = 1;
- 					
- 					if (there[0]> here[0]) var xinc = -1;
- 					else var xinc = 1;
- 					
- 					var x = there[0] + xinc;
- 					var y = there[1] + yinc;
- 					while (x!= here[0] && y != here[1]) 
- 						{
- 							console.log('checking (%o,%o)', x, y);
- 							if(Game.getPieceAt( [x, y]) != null)
- 								return false;
- 								
- 								x+=xinc; y+=yinc;
- 						}
- 						
- 						return true;
- 				break;
- 			}
- 			case("king"):{
- 				if(Math.abs(here[0] - there[0]) <= 1 && Math.abs(here[1] - there[1]) <= 1)
- 					return true;
- 				return false;
- 				break;
- 			}
- 			case("pawn"):{
-
- 				if(color == 0)
- 					var forward = 1;
- 				else
- 					var forward = -1
- 					
- 						if(    there[0]==here[0] 
- 							&& there[1] == here[1] + forward
- 							&& Game.getPieceAt(there) == null)
- 							return true;
- 							
- 						if(    (there[0]==here[0]+1 || there[0]==here[0]-1) 
- 						    && there[1] == here[1] + forward 
- 						    && Game.getPieceAt(there) != null)
- 						    return true;
- 						
- 						if(    there[0]==here[0] 
- 							&& there[1] == here[1] + forward + forward
- 							&& Game.getPieceAt( [ here[0], here[1]+forward ] ) == null
- 							&& here[1] == 3.5 - (2.5 * forward) )
- 							return true;
- 					
- 					return false;
- 			break;
- 			}
- 		}
- 		}
-
 function clone(obj) {
 	if (null == obj || "object" != typeof obj) return obj;
 	var copy = obj.constructor();
@@ -335,6 +169,135 @@ var Game = {
 		Game.drawPiece(piece.type, piece.color, pos);
 	},
 
+	//given a piece and a target tuple, can that piece go there?
+	validateMove: function (type, here, there, color) {
+		if (here[0] == there[0] && here[1] == there[1]) return true;
+		if (Game.getPieceAt(there) != null && Game.getPieceAt(there).color == color) return false;
+
+		switch (type) { //Game.getPieceAt(grid)
+		case ("rook"):
+			{
+				if (here[0] == there[0]) {
+					if (there[1] > here[1]) var inc = -1;
+					else var inc = 1;
+
+					for (var y = there[1] + inc; y != here[1]; y += inc) {
+						if (Game.getPieceAt([here[0], y]) != null) return false;
+					}
+					return true;
+				}
+
+				if (here[1] == there[1]) {
+					if (there[0] > here[0]) var inc = -1;
+					else var inc = 1;
+
+					for (var x = there[0] + inc; x != here[0]; x += inc) {
+						if (Game.getPieceAt([x, here[1]]) != null) return false;
+					}
+					return true;
+				}
+
+				return false;
+				break;
+			}
+		case ("knight"):
+			{
+				if (there[0] == here[0] + 2 || there[0] == here[0] - 2) if (there[1] == here[1] + 1 || there[1] == here[1] - 1) return true;
+				if (there[1] == here[1] + 2 || there[1] == here[1] - 2) if (there[0] == here[0] + 1 || there[0] == here[0] - 1) return true;
+				return false;
+				break;
+			}
+		case ("bishop"):
+			{
+				if (!(Math.abs(here[0] - there[0]) == Math.abs(here[1] - there[1]))) return false;
+
+				if (there[1] > here[1]) var yinc = -1;
+				else var yinc = 1;
+
+				if (there[0] > here[0]) var xinc = -1;
+				else var xinc = 1;
+
+				var x = there[0] + xinc;
+				var y = there[1] + yinc;
+
+				while (x != here[0] && y != here[1]) {
+					console.log('checking (%o,%o)', x, y);
+					if (Game.getPieceAt([x, y]) != null) return false;
+
+					x += xinc;
+					y += yinc;
+				}
+
+				return true;
+				break;
+			}
+		case ("queen"):
+			{
+				//rook-like movement
+				if (here[0] == there[0]) {
+					if (there[1] > here[1]) var inc = -1;
+					else var inc = 1;
+
+					for (var y = there[1] + inc; y != here[1]; y += inc) {
+						if (Game.getPieceAt([here[0], y]) != null) return false;
+					}
+					return true;
+				}
+
+				if (here[1] == there[1]) {
+					if (there[0] > here[0]) var inc = -1;
+					else var inc = 1;
+
+					for (var x = there[0] + inc; x != here[0]; x += inc) {
+						if (Game.getPieceAt([x, here[1]]) != null) return false;
+					}
+					return true;
+				}
+				//bishop-like movement
+				if (!(Math.abs(here[0] - there[0]) == Math.abs(here[1] - there[1]))) return false;
+
+				if (there[1] > here[1]) var yinc = -1;
+				else var yinc = 1;
+
+				if (there[0] > here[0]) var xinc = -1;
+				else var xinc = 1;
+
+				var x = there[0] + xinc;
+				var y = there[1] + yinc;
+				while (x != here[0] && y != here[1]) {
+					console.log('checking (%o,%o)', x, y);
+					if (Game.getPieceAt([x, y]) != null) return false;
+
+					x += xinc;
+					y += yinc;
+				}
+
+				return true;
+				break;
+			}
+		case ("king"):
+			{
+				if (Math.abs(here[0] - there[0]) <= 1 && Math.abs(here[1] - there[1]) <= 1) return true;
+				return false;
+				break;
+			}
+		case ("pawn"):
+			{
+				if (color == 0) var forward = 1;
+				else var forward = -1
+
+				if (there[0] == here[0] && there[1] == here[1] + forward && Game.getPieceAt(there) == null) return true;
+
+				if ((there[0] == here[0] + 1 || there[0] == here[0] - 1) && there[1] == here[1] + forward && Game.getPieceAt(there) != null) return true;
+
+				if (there[0] == here[0] && there[1] == here[1] + forward + forward && Game.getPieceAt([here[0], here[1] + forward]) == null && here[1] == 3.5 - (2.5 * forward)) return true;
+
+				return false;
+				break;
+			}
+		}
+	},
+
 	placePiece: function (event) {
 		var cancelMove = false;		
 		var pos = Game.getPosition(event);
@@ -345,7 +308,7 @@ var Game = {
 		Game.blackCheck = false;
 		Game.redCheck = false;
 		
-		cancelMove = !(validateMove(Game.previous.piece.type, Game.previous.grid, grid, Game.previous.piece.color))
+		cancelMove = !(Game.validateMove(Game.previous.piece.type, Game.previous.grid, grid, Game.previous.piece.color))
 		
 		if (cancelMove)
 			return;
@@ -527,7 +490,7 @@ var Game = {
 			var checkPiece = allPieces[i][0]; 
 			if (checkPiece == Game.blackKing) continue;
 			if (checkPiece.color == 0) continue;
-			if (validateMove(checkPiece.type, allPieces[i][1], blackKingGrid, checkPiece.color))
+			if (Game.validateMove(checkPiece.type, allPieces[i][1], blackKingGrid, checkPiece.color))
 				{
 				Game.blackCheck = true;
 				break;
@@ -539,7 +502,7 @@ var Game = {
 			var checkPiece = allPieces[i][0]; 
 			if (checkPiece == Game.redKing) continue;
 			if (checkPiece.color == 1) continue;
-			if (validateMove(checkPiece.type, allPieces[i][1], redKingGrid, checkPiece.color))
+			if (Game.validateMove(checkPiece.type, allPieces[i][1], redKingGrid, checkPiece.color))
 				{
 				Game.redCheck = true;
 				break;
@@ -583,7 +546,7 @@ var Game = {
 					for(var y = 0; y<8; y++)
 					{
 					
-						if(validateMove(piece.type, where, [x,y], piece.color))
+						if(Game.validateMove(piece.type, where, [x,y], piece.color))
 						{
 							if([x,y]!=redKingGrid && [x,y] != blackKingGrid) 
 							{
